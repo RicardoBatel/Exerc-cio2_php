4ef713fb025d2526c56ec24b76aa32536fee3ee0
Ricardo Batel <ricardo.batel@hotmail.com>
2022-11-08 11:42:17 -0300

    melhorando a vizualização do resultado

--- exercicio2.php
+++ exercicio2.php
@@ -16,19 +16,21 @@
 					$imposto = 0;
 				}elseif ($salario >= 1903.99 && $salario <= 2826.65) {
 					$imposto = ($salario * 7.5) / 100;
+					echo "O valor da aliquota é de 7,5%, então o Imposto de Renda será no valor de R$ $imposto";
 				}elseif ($salario >= 2826.66 && $salario <= 3751.05) {
 					$imposto = ($salario * 15) / 100;
+					echo "O valor da aliquota é de 15%, então o Imposto de Renda será no valor de R$ $imposto";
 				}elseif ($salario >= 3751.06 && $salario <= 4664.68) {
 					$imposto = ($salario * 22.5) / 100;
+					echo "O valor da aliquota é de 22,5%, então o Imposto de Renda será no valor de R$ $imposto";
 				}elseif ($salario >= 4664.69) {
 					$imposto = ($salario * 27.5) / 100;
+					echo "O valor da aliquota é de 27,5%, então o Imposto de Renda será no valor de R$ $imposto";
 				}
-
-				return $imposto;
 			
 			}
 
-			echo calculoIR(5000);
+			calculoIR(5000);
 
 		?>
 
