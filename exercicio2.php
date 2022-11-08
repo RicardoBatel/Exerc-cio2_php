ea2754329cc7c6ceef3a09ab4629d2bfde03d64a
Ricardo Batel <ricardo.batel@hotmail.com>
2022-11-08 11:30:11 -0300

    primeiro commit

---
+++ exercicio2.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Curso PHP</title>
+	</head>
+	<body>
+		
+		<?php 
+
+			function calculoIR($salario) {
+				$imposto = 0;
+
+				if($salario <= 1903.98) {
+					$imposto = 0;
+				}elseif ($salario >= 1903.99 && $salario <= 2826.65) {
+					$imposto = ($salario * 7.5) / 100;
+				}elseif ($salario >= 2826.66 && $salario <= 3751.05) {
+					$imposto = ($salario * 15) / 100;
+				}elseif ($salario >= 3751.06 && $salario <= 4664.68) {
+					$imposto = ($salario * 22.5) / 100;
+				}elseif ($salario >= 4664.69) {
+					$imposto = ($salario * 27.5) / 100;
+				}
+
+				return $imposto;
+			
+			}
+
+			echo calculoIR(5000);
+
+		?>
+
+
+	</body>
+</html>
